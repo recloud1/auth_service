86ea4851b24d62dcf5eca5d5288370319a9fdfb7
Dan <babindj@uriit.ru>
2022-10-30 14:22:26 +0500

    Добавление swagger при запуске продовой версии

--- app/run.py
+++ app/run.py
@@ -2,10 +2,14 @@ from core.config import envs
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
 
+from core.swagger import api
+
 monkey.patch_all()
 
 
 from main import app
 
 http_server = WSGIServer(('0.0.0.0', envs.app.port), app)
+
+api.register(app)
 http_server.serve_forever()
